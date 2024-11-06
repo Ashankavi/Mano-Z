541a457b19a653a3faa8a146a7afa5a2637ca5a7
Ashan Kavindu <134072313+Ashankavi@users.noreply.github.com>
2024-11-06 11:11:35 +0530

    Project Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Hero from "./Components/Hero"
+import NewArivale from "./Components/NewArivale"
 
 function App() {
 
   return (
       <div>
         <Hero/>
+        <NewArivale/>
 
       </div>
   )
